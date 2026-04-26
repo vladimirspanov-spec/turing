ef86736001ee74bcd3488ae82628aba341a71929
Vladimir <vladimir.s.panov@gmail.com>
2026-04-26 20:57:23 +0300

    Сделал таблицу и возможность запускать машину

--- turing.cpp
+++ turing.cpp
@@ -4,11 +4,23 @@
 #include <QPushButton>
 #include <QDebug>
 #include <QString>
+#include <map>
+#include <sstream>
+#include <thread>
+#include <chrono>
+
+QVBoxLayout *table_layout = new QVBoxLayout;
+std::map<int, char> symbols;
+std::map<char, int> rsymbols;
 
 turing::turing(std::string alphabet, std::string extrasymbols) {
     this->setFixedSize(700, 500);
     header = new int;
     *header = -1;
+
+    timer = new QTimer(this);
+    timer->setInterval(1000);
+    connect(timer, &QTimer::timeout, this, &turing::step);
     /*this->Alphabet = new QLineEdit(alphabet.c_str());
     Alphabet->setReadOnly(true);
     this->ExtraSymbols = new QLineEdit(extrasymbols.c_str());
@@ -38,21 +50,232 @@ turing::turing(std::string alphabet, std::string extrasymbols) {
     //ribbon[3].setText("a");
     //ribbon[3].setStyleSheet("QLineEdit { border: 2px solid #00EEAA }");
     main_layout->addLayout(ribbon_layout);
+
+
+    int x = 0;
+    for (unsigned i = 0; i < alphabet.size(); ++i) {
+        symbols[x] = alphabet[i];
+        rsymbols[alphabet[i]] = x + 1;
+        x++;
+    }
+    symbols[x] = '^';
+    rsymbols['^'] = x+1;
+    x++;
+    for (unsigned i = 0; i < extrasymbols.size(); ++i) {
+        symbols[x] = extrasymbols[i];
+        rsymbols[extrasymbols[i]]= x + 1;
+        x++;
+    }
+    al_size = new int;
+    *al_size = x;
+
+    table_title = new QLineEdit[x+1];
+    QHBoxLayout *table_title_layout = new QHBoxLayout;
+    //тут был table_layout
+    table_title[0].setText("Состояние");
+    table_title[0].setReadOnly(true);
+    table_title_layout->addWidget(&table_title[0]);
+    for (int i = 1; i <= x; ++i) {
+        table_title[i].setText(QString(symbols[i-1]));
+        table_title[i].setReadOnly(true);
+        table_title_layout->addWidget(&table_title[i]);
+    }
+    table_layout->setSpacing(0);
+    table_layout->setContentsMargins(0, 0, 0, 0);
+    table_layout->addLayout(table_title_layout);
+
+
+    table = new QVector<QLineEdit*>;
+    lines = new QVector<QHBoxLayout*>;
+    lines->append(new QHBoxLayout);
+    q_header = new int;
+    *q_header = 0;
+
+
+    table->append(new QLineEdit("q0"));
+    table->at(0)->setReadOnly(true);
+    table->at(0)->setStyleSheet("QLineEdit { border: 2px solid #0000AA }");
+    lines->at(0)->addWidget(table->at(0));
+    for (int i = 1; i <= x; ++i) {
+        table->append(new QLineEdit(""));
+        lines->at(0)->addWidget(table->at(i));
+    }
+    table_layout->addLayout(lines->at(0));
+
+
+    QPushButton *plusq, *minusq;
+    plusq = new QPushButton("+");
+    minusq = new QPushButton("-");
+    connect(plusq, &QPushButton::clicked, this, &turing::add_q);
+    connect(minusq, &QPushButton::clicked, this, &turing::delete_q);
+    QHBoxLayout *add_q_layout = new QHBoxLayout;
+    add_q_layout->addWidget(plusq); add_q_layout->addWidget(minusq);
+
+
+    main_layout->addLayout(table_layout);
+    main_layout->addLayout(add_q_layout);
+
+
+    QPushButton *play_button = new QPushButton("Запустить");
+    QPushButton *step_button = new QPushButton("Шаг");
+    QPushButton *stop_button = new QPushButton("Стоп");
+    QHBoxLayout *play_layout = new QHBoxLayout;
+    play_layout->addWidget(play_button); play_layout->addWidget(step_button); play_layout->addWidget(stop_button);
+    main_layout->addLayout(play_layout);
+    connect(step_button, &QPushButton::clicked, this, &turing::step);
+    connect(play_button, &QPushButton::clicked, this, &turing::play);
+    connect(stop_button, &QPushButton::clicked, this, &turing::stop);
+    stopped = new int;
+    *stopped = 0;
+
     setLayout(main_layout);
     connect(set_str, &QPushButton::clicked, this, &turing::setNewStr);
 }
 
 void turing::setNewStr() {
+    for (int i = 0; i < 17; i++) {
+        ribbon[i].setText("^");
+        ribbon[i].setStyleSheet("");
+    }
     std::string s = str->text().toStdString();
     int x = (17-s.size())/2;
     int j = 0;
     for (unsigned long i = x; i < x +s.size(); ++i) {
         ribbon[i].setText(QString(str->text().at(j)));
         ++j;
-        qDebug() << "lol";
+        //qDebug() << "lol";
     }
-    //qDebug() << s.size();
     *header = (17-s.size())/2;
     ribbon[(17-s.size())/2].setStyleSheet("QLineEdit { border: 2px solid #00EEAA }");
-    //qDebug() << (17-s.size())/2;
+    *stopped = 0;
+}
+
+void turing::add_q() {
+    std::stringstream ss;
+    qDebug() << *al_size;
+    int index = lines->size();
+    ss << 'q' << index;
+    lines->append(new QHBoxLayout);
+    table->append(new QLineEdit(QString::fromStdString(ss.str())));
+    table->at(table->size()-1)->setReadOnly(true);
+    lines->at(index)->addWidget(table->at(table->size()-1));
+    for (int i = 1; i <= *al_size; ++i) {
+        table->append(new QLineEdit(""));
+        lines->at(index)->addWidget(table->at(table->size()-1));
+    }
+    table_layout->addLayout(lines->at(index));
+}
+
+
+void turing::delete_q() {
+    if (lines->size() <= 1) {
+        qDebug() << "Нельзя удалить последнюю строку";
+        return;
+    }
+
+    // Получаем последний layout
+    QHBoxLayout* lastLayout = lines->last();
+    if (!lastLayout) return;
+
+    // Удаляем все виджеты из последнего layout
+    while (lastLayout->count() > 0) {
+        QWidget* widget = lastLayout->itemAt(0)->widget();
+        if (widget) {
+            lastLayout->removeWidget(widget);
+            delete widget;
+        }
+    }
+
+    // Удаляем последний layout из table_layout
+    table_layout->removeItem(lastLayout);
+
+    // Удаляем последний layout из вектора lines
+    lines->pop_back();
+    delete lastLayout;
+
+    // Удаляем соответствующие QLineEdit из вектора table
+    int itemsToRemove = *al_size + 1;
+    for (int i = 0; i < itemsToRemove && !table->isEmpty(); ++i) {
+        table->pop_back();
+    }
+    if (*q_header >= table->size()) {
+        *q_header = 0;
+        table->at(0)->setStyleSheet("QLineEdit { border: 2px solid #0000AA }");
+    }
+    qDebug() << *q_header << " " << table->size();
+    qDebug() << "Осталось строк:" << lines->size();
+}
+
+void turing::step() {
+    if (*header == -1) { qDebug() << "no"; return; }
+    if (*stopped) { qDebug() << "machine stopped"; return; }
+    char sym = *ribbon[*header].text().toLatin1();
+    if (rsymbols[sym] == 0) { return; }
+    std::string s = table->at(rsymbols[sym] + *q_header)->text().toStdString(), new_sym, dir, new_q;
+    std::stringstream ss(s);
+    std::string s1;
+    ss >> s1;
+    if (s1.size() == 1 && s1 != "<" && s1 != ">") {
+        new_sym = s1;
+        ss >> s1;
+    }
+    if (s1 == "<" || s1 == ">") {
+        dir = s1;
+        ss >> s1;
+    }
+    if ((s1[0] == 'q' && s1.size() > 1) || s1 == "!") {
+        new_q = s1;
+    }
+
+    if (new_sym != "") {
+        ribbon[*header].setText(QString::fromStdString(new_sym));
+    }
+
+
+    if (dir == ">") {
+        ribbon[*header].setStyleSheet("");
+        *header += 1;
+        ribbon[*header].setStyleSheet("QLineEdit { border: 2px solid #00EEAA }");
+    } else if(dir =="<") {
+        ribbon[*header].setStyleSheet("");
+        *header -= 1;
+        ribbon[*header].setStyleSheet("QLineEdit { border: 2px solid #00EEAA }");
+    }
+
+    if (new_q == "!") {
+        *stopped = 1;
+        timer->stop();
+    }
+    else if (new_q != "") {
+        new_q.erase(0, 1);
+        int q = std::stoi(new_q);
+        table->at(*q_header)->setStyleSheet("");
+        *q_header = q * (*al_size + 1);
+        table->at(*q_header)->setStyleSheet("QLineEdit { border: 2px solid #0000AA }");
+        qDebug() << *q_header;
+    }
+
+    //qDebug() << QString::fromStdString(new_q);
+
+    //qDebug() << table->at(rsymbols[sym])->text().toStdString();
+
+}
+
+void turing::play() {
+    if (*stopped) {
+        qDebug() << "Машина уже остановлена";
+        return;
+    }
+    if (timer->isActive()) {
+        timer->stop();
+    }
+    timer->start(); // Запускаем таймер
+    qDebug() << "Запуск машины с интервалом 2 секунды";
+}
+
+void turing::stop() {
+    if (timer->isActive()) {
+        timer->stop();
+        qDebug() << "Машина остановлена пользователем";
+    }
 }
--- turing.h
+++ turing.h
@@ -4,6 +4,9 @@
 #include <QWidget>
 #include <QLineEdit>
 #include <string>
+#include <QVBoxLayout>
+#include <QVector>
+#include <QTimer>
 
 class turing : public QWidget
 {
@@ -12,15 +15,31 @@ public:
     turing(std::string alphabet, std::string extrasymbols);
     ~turing() {
         delete header;
+        delete al_size;
+        delete q_header;
+        delete stopped;
     }
 private slots:
     void setNewStr();
+    void add_q();
+    void delete_q();
+    void step();
+    void play();
+    void stop();
 private:
     QLineEdit *Alphabet;
     QLineEdit *ExtraSymbols;
     QLineEdit *ribbon;
     QLineEdit *str;
     int *header;
+    int *q_header;
+    int *al_size;
+    int *stopped;
+    QVector<QLineEdit*> *table;
+    QVector<QHBoxLayout*> *lines;
+    QLineEdit *table_title;
+    QTimer *timer;
+
 };
 
 #endif // TURING_H
